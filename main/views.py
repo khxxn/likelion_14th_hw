b4af1f38380476c4cd18ac47aa8561d09b5a3aaa
khxxn <kyohyun58@naver.com>
2026-04-03 16:25:29 +0900

    2주차 과제

--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def mainpage(request):
+    return render(request,'main/mainpage.html')
+
+def secondpage(request):
+    context = {
+        'info' : {
+            'name': '구교현',
+            'nickname': '구구콘 🍦',
+            'mbti': 'ISTP',
+            'hobby': '야구 시청 (LG 트윈스)',
+            'insta': 'khxxn_9'
+        }
+    }
+    return render(request,'main/secondpage.html', context)
